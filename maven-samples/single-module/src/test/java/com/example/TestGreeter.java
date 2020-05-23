a68d72c2037f131e29f709c52f083517b84544bc
HarilalOP <harilal1990@gmail.com>
2020-05-23 20:56:24 +0300

    Fixed issue in test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
 
   @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeLunchMessage() {
     String someone = "World, Thank you for bringing me lunch";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
